97dfabd74bdbbd512c71956fb8a5e4120dc26e39
steelwolf180 <steelwolf180@gmail.com>
2017-07-07 15:50:29 +0800

    Render default contacts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import ListContacts from "./ListContacts"
+
+const contacts = [
+{
+  "id": "ryan",
+  "name": "Ryan Florence" ,
+  "email": "ryan@reacttraining.com",
+  "avatarURL": "http://localhost:5001/ryan.jpg"
+},
+{
+  "id": "michael",
+  "name": "Micheal Jackson" ,
+  "email": "micheal@reacttraining.com",
+  "avatarURL": "http://localhost:5001/michael.jpg"  
+},
+{
+  "id": "tyler",
+  "name": "Tyler MrGinnis" ,
+  "email": "tyler@reacttraining.com",
+  "avatarURL": "http://localhost:5001/tyler.jpg"
+}
+
+]
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
+    render() {
+      return (
+        <div>
+          <ListContacts contacts={contacts} />
+        </div>
+      )
+    }
   }
-}
 
 export default App;
